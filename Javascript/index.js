c2c9611fb42ab7c2e54588ccd17395e0f2819e07
Khalil Ahmad <iamkhalil2005@gmail.com>
2025-02-10 00:06:58 +0500

    added JS Class code

---
+++ Javascript/index.js
@@ -0,0 +1,99 @@
+// Variables
+// var, let, const
+
+// var test=23; //int value
+// // test="ali"; //string value
+// // console.log(test) //printed correct
+// const test3="23";
+// console.log(test==test3);
+
+// const test2="ali";  //string value
+// test2=23; //int value
+// console.log(test2) //error cuz its constant.
+
+// string, number, boolean, null, undefined, symbol, bigint
+// object, array, function
+
+// Primitive(Already defined fully), Non-primitive(defined, but user made)
+
+//Operators: +,-,*,/,%,==,===,!=,!==
+
+
+// Diff between ==, ===
+let a="20"; //string
+let b=20; //number
+
+// console.log(a===b);
+
+//Functions
+// function Greet(name, age){
+//     // console.log("Hello");
+//     console.log(`Hello ${name}. I am ${age} years old.`)
+// }
+// Greet("Oggy",23);
+// Greet("Oggy2","2");
+
+
+//  Loops
+
+// array
+let numbers=[1,2,3,4,5,6,7];
+
+// for(initialization, condition, increment){
+//     code to execute
+// }
+
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+// }
+
+
+// let i=7;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let j=7;
+// do{
+//     console.log(j);
+//     j++;
+// }
+// while(j<=5);
+
+
+// Loop1:
+// service="Developer", print
+// Loop2:
+// service="hacker", print
+// let services=["Developer","hacker","Memer","Joker"];
+// for(let service of services){
+//     console.log(service);
+// }
+
+
+
+// Loop through Objects:
+// let client={
+//     name:"Oggy", age:25, city:"Lahore", amount:30000
+// }
+
+// for (let key in client){
+//     console.log(key+": ",client[key]);
+// }
+
+
+
+// if else
+// Ternary Operator => condition ? true : false
+// let age=1;
+// let message= age>=18 ? "You can vote" : "You cannot vote"
+// console.log(message);
+
+
+
+
+
+
+
